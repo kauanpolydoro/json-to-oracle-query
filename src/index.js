74b8e8a18deacc75ea2100174971622b8c26d55a
Kauan Polydoro <kauan.2812@hotmail.com>
2022-11-10 08:12:01 -0300

    feature: JSON_ARRAY & JSON_ARRAYAGG

--- src/index.js
+++ src/index.js
@@ -3,21 +3,23 @@ const { join } = require('path');
 
 (async () => {
   const sourceFile = join(__dirname, "..", "source.json");
-  const destFile = join(__dirname, "..", "dest.json");
+  const destFile = join(__dirname, "..", "dest.sql");
 
   let content = await File.read(sourceFile);
 
+  content = content.replace(/("\w+")\s*:\s*\[\s*{/gi, `$1 VALUE JSON_ARRAYAGG({`);
   content = content.replace(/{/gi, `JSON_OBJECT (`);
-  content = content.replace(/}/gi, `RETURNING CLOB)`);
-  content = content.replace(/"(\w+)":\s".+"/gi, `'$1' VALUE '#$1#'`);
+  content = content.replace(/}|\]/gi, `RETURNING CLOB)`);
+  content = content.replace(/"(\w+)":\s*".+"/gi, `'$1' VALUE '#$1#'`);
   content = content.replace(/"(\w+)"\s*:\s*\d+/gi, `'$1' VALUE 999999`);
-  content = content.replace(/"(\w+)"\s*:\s*(FALSE|TRUE|false|true)/gi, `'$1' VALUE '$2' format json`);
-  content = content.replace(/"(\w+)": JSON_OBJECT \(/gi, `'$1' VALUE JSON_OBJECT (`);
-  content = content.replace(/"(\w+)"\s*:\s*\[/gi, `'$1' VALUE JSON_ARRAYAGG (`);
-  content = content.replace(/]/gi, `RETURNING CLOB)`);
+  content = content.replace(/"(\w+)"\s*:\s*(false|true)/gi, `'$1' VALUE '$2' format json`);
+  content = content.replace(/"(\w+)"\s*:\s*JSON_OBJECT \(/gi, `'$1' VALUE JSON_OBJECT (`);
+  content = content.replace(/"(\w+)"\s*:\s*\[/gi, `'$1' VALUE JSON_ARRAY (`);
   content = content.replace(/"/gi, `'`);
   content = content.replace(/:/gi, `value`);
+  content = 'SELECT'.concat(' ', content);
+  content = content.concat(' ', 'FROM DUAL;');
 
   File.write(destFile, content);
- 
+
 })();
